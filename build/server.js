3f21ee76c2d40708ae1d689089cb0b4c652b0355
fenghengzhi <BejzbPKgU!^92wkg>
2018-07-08 18:45:19 +0800

    edit

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.get('/articles', (request, response) => response.send('文章列表'));
-app.post('/articles', (request, response) => response.send('新增文章'));
-app.put('/articles', (request, response) => response.send('修改文章'));
-app.delete('/articles', (request, response) => response.send('删除文章'));
-app.listen(8080);
