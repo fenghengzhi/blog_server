25e01c9274fc517bcfb7f8a6585a9e32c0f78c8e
fenghengzhi <BejzbPKgU!^92wkg>
2018-07-11 23:00:20 +0800

    edit

--- src/controllers/article/Article.ts
+++ src/controllers/article/Article.ts
@@ -2,38 +2,38 @@ import * as Sequelize from 'sequelize';
 import db from '../../db';
 
 interface ArticleAttributes {
-  id?: number;
-  title: string;
-  content: string;
-  readNum?: number;
-  deleted?: boolean;
-  createdAt?: string;
-  updatedAt?: string;
+    id?: number;
+    title: string;
+    content: string;
+    readNum?: number;
+    deleted?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
 }
 
 type ArticleInstance = Sequelize.Instance<ArticleAttributes> & ArticleAttributes;
 
 const Article = db.define<ArticleInstance, ArticleAttributes>('article', {
-  id       : { //自增长id,主键,整形
-    type         : Sequelize.INTEGER,
-    primaryKey   : true,
-    autoIncrement: true,
-  },
-  title    : {
-    type: Sequelize.STRING,
-  },
-  content  : {
-    type: Sequelize.TEXT,
-  },
-  readNum  : {
-    type        : Sequelize.BIGINT,
-    defaultValue: 0,
-  },
-  deleted  : {
-    type        : Sequelize.BOOLEAN,
-    defaultValue: false,
-  },
-});
+    id: { //自增长id,主键,整形
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+    },
+    content: {
+        type: Sequelize.TEXT,
+    },
+    readNum: {
+        type: Sequelize.BIGINT,
+        defaultValue: 0,
+    },
+    deleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+    },
+}, {charset: 'utf8'});
 
 
 // force: true will drop the table if it already exists
